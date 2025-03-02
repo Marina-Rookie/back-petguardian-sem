c6552cb08bb026cb80106b0ad4f6532910fb2021
Marina.p <125839912+Marina-Rookie@users.noreply.github.com>
2025-03-02 01:02:33 -0300

    cors

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
-const express = require('express');
-const multer = require('multer');
-const connection = require('./config/conectarDB.js');
-const cors = require('cors');
-const { swaggerUi, specs } = require('./utils/swaggerConfig');
+const express = require("express");
+const multer = require("multer");
+const connection = require("./config/conectarDB.js");
+const cors = require("cors");
+const { swaggerUi, specs } = require("./utils/swaggerConfig");
 const port = process.env.PORT || 3000;
-const router = require('./routes/routes.js');
-const tareaProgramada = require('./utils/tareaProgramada.js');
+const router = require("./routes/routes.js");
+const tareaProgramada = require("./utils/tareaProgramada.js");
 
-require('dotenv').config();
+require("dotenv").config();
 
 connection.conectarDB();
 
-
 const app = express();
 app.use(express.json());
-app.use(cors());
-app.use(router); 
+
+// Enable CORS for specific origins
+const corsOptions = {
+  origin: "http://localhost:4200", // Replace with your frontend URL
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(router);
 
 // Configuración de multer
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 // Configuración de Swagger
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 app.listen(port, () => {
   tareaProgramada.programarUpdateReservas();
   console.log(`Servidor escuchando en http://localhost:${port}`);
 });
 
-router.get('/', (req, res) => {
-  res.send('Bienvenido a la API de PetGuardian');
+router.get("/", (req, res) => {
+  res.send("Bienvenido a la API de PetGuardian");
 });
